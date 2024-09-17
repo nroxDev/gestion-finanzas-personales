30c23198c3f82f34cd08b8215fc0b2be8e248d39
Roxana <roxana@MacBook-Air-de-Roxana.local>
2024-09-18 01:06:43 +0200

    implemntación basica del registro

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import Registro from './componentes/registro/registro';
 import Login from './componentes/login/login';
 import Aplicacion from './componentes/aplicacion/aplicacion';
