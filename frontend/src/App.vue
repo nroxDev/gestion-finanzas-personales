5337a7ad8c216aae0ad7b1cca570c48253786cd6
Roxana <nolvia.roxana.aguilera@gmail.com>
2024-11-06 00:31:55 +0100

    implementa el componente InputTexto

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Rox App"/>
+  <h1>APP</h1>
+     <InputTexto texto="Nombre de usuario"/>
+     <InputTexto texto="Correo electronico"/>
+     <InputTexto texto="Contraseña"/>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import InputTexto from './components/InputTexto.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   
+    InputTexto
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ frontend/src/components/InputTexto.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="contenedor">
+        <label class="texto">{{ texto }}</label>
+        <input class="input" type="text">
+    </div>
+</template>
+
+<style>
+.texto {
+    color: #030303;
+    font-size: 16px;
+    font-family: "Open Sans";
+    line-height: 24px;
+}
+
+.input {
+    width: 436px;
+    height: 46px;
+    padding: 0px 8px;
+    border: 1px solid #d6d6d6;
+    box-sizing: border-box;
+    border-radius: 24px;
+    box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.08);
+    color: #94a3b8;
+    font-size: 14px;
+    font-family: "Open Sans";
+    line-height: 46px;
+    outline: none;
+}
+
+.contenedor {
+    display: flex;
+    flex-direction: column;
+    padding: 16px;
+}
+</style>
+
+
+<script>
+export default {
+    name: "InputTexto",
+    props: {
+        texto: String,
+    }
+}
+
+</script>
