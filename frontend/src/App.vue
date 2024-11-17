84003f787cd38323b8a8161a0b9286a0b63e9b67
Roxana <nolvia.roxana.aguilera@gmail.com>
2024-11-17 22:32:17 +0100

    añade el componente CheckboxOpcions

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,6 +4,12 @@
     <InputTexto texto="Nombre de usuario" />
     <InputTexto texto="Correo electronico" />
     <InputTexto texto="Contraseña" />
+
+    <CheckboxOpcions texto="Sexo:" opcion1="M" opcion2="F"/>
+
+    <CheckboxOpcions texto="MAcota favorita:" opcion1="pirulo" opcion2="Kismet"/>
+
+
     <BotonFormulario texto="Registrase" />
   </CajaSombreada>
 
@@ -13,13 +19,15 @@
 import InputTexto from './components/InputTexto.vue';
 import BotonFormulario from './components/BotonFormulario.vue';
 import CajaSombreada from './components/CajaSombreada.vue';
+import CheckboxOpcions from './components/CheckboxOpcions.vue';
 
 export default {
   name: 'App',
   components: {
     InputTexto,
     BotonFormulario,
-    CajaSombreada
+    CajaSombreada,
+    CheckboxOpcions
   }
 }
 </script>
---
+++ frontend/src/components/CheckboxOpcions.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <p>{{ texto }}</p>
+
+        {{ opcion1 }} <input type="checkbox">
+        
+        {{ opcion2 }}<input type="checkbox">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CheckboxOpciones",
+    props: {
+        texto: String,
+        opcion1: String,
+        opcion2: String,
+    },
+}
+</script>
+
+<style></style>
