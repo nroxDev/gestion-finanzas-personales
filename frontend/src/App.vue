17e6ea79ac14dba93b8eac65819857644197a5fb
Roxana <nolvia.roxana.aguilera@gmail.com>
2024-11-17 22:09:01 +0100

    añade el componente BotonFormulario

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,17 +3,19 @@
      <InputTexto texto="Nombre de usuario"/>
      <InputTexto texto="Correo electronico"/>
      <InputTexto texto="Contraseña"/>
+     <BotonFormulario texto="Registrase"/>
 
 </template>
 
 <script>
 import InputTexto from './components/InputTexto.vue';
+import BotonFormulario from './components/BotonFormulario.vue';
 
 export default {
   name: 'App',
   components: {
-   
-    InputTexto
+    InputTexto,
+    BotonFormulario
   }
 }
 </script>
---
+++ frontend/src/components/BotonFormulario.vue
@@ -0,0 +1,33 @@
+<template>
+    <button class="boton-formulario">{{ texto }}</button>
+</template>
+
+
+<script>
+export default {
+    name: 'BotonFormulario',
+    props: {
+        texto: String,
+    }
+}
+</script>
+
+
+<style>
+.boton-formulario {
+    cursor: pointer;
+    width: 436px;
+    height: 44px;
+    padding: 0px 8px;
+    border: 0;
+    box-sizing: border-box;
+    border-radius: 24px;
+    background-color: #d40100;
+    color: #ffffff;
+    font-size: 14px;
+    font-family: "Open Sans";
+    font-weight: 600;
+    line-height: 20px;
+    outline: none;
+}
+</style>
