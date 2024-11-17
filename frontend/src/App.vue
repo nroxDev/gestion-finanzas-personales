4d5720c1a3fd4eb416a2a2f0b9b31d1a89cadb80
Roxana <nolvia.roxana.aguilera@gmail.com>
2024-11-17 22:18:17 +0100

    añade el componente CajaSombreada

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,25 +1,27 @@
 <template>
-  <h1>APP</h1>
-     <InputTexto texto="Nombre de usuario"/>
-     <InputTexto texto="Correo electronico"/>
-     <InputTexto texto="Contraseña"/>
-     <BotonFormulario texto="Registrase"/>
+  <CajaSombreada>
+    <h1>APP</h1>
+    <InputTexto texto="Nombre de usuario" />
+    <InputTexto texto="Correo electronico" />
+    <InputTexto texto="Contraseña" />
+    <BotonFormulario texto="Registrase" />
+  </CajaSombreada>
 
 </template>
 
 <script>
 import InputTexto from './components/InputTexto.vue';
 import BotonFormulario from './components/BotonFormulario.vue';
+import CajaSombreada from './components/CajaSombreada.vue';
 
 export default {
   name: 'App',
   components: {
     InputTexto,
-    BotonFormulario
+    BotonFormulario,
+    CajaSombreada
   }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
---
+++ frontend/src/components/CajaSombreada.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="caja-sombreada">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CajaSombreada",
+    props: {}
+}
+</script>
+
+
+<style>
+.caja-sombreada {
+  background-color: #ffffff;
+  border-radius: 24px;
+  box-shadow: 2px 0px 10px rgba(3,3,3,0.1);
+  padding: 16px;
+}
+</style>
