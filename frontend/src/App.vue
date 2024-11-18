8adc9863e67f6606f10bb886456805a70c549b92
Roxana <nolvia.roxana.aguilera@gmail.com>
2024-11-19 00:42:02 +0100

    añade el componente del formulario de registro

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,33 +1,15 @@
 <template>
-  <CajaSombreada>
-    <h1>APP</h1>
-    <InputTexto texto="Nombre de usuario" />
-    <InputTexto texto="Correo electronico" />
-    <InputTexto texto="Contraseña" />
-
-    <CheckboxOpcions texto="Sexo:" opcion1="M" opcion2="F"/>
-
-    <CheckboxOpcions texto="MAcota favorita:" opcion1="pirulo" opcion2="Kismet"/>
-
-
-    <BotonFormulario texto="Registrase" />
-  </CajaSombreada>
+    <FormularioRegistro/>
 
 </template>
 
 <script>
-import InputTexto from './components/InputTexto.vue';
-import BotonFormulario from './components/BotonFormulario.vue';
-import CajaSombreada from './components/CajaSombreada.vue';
-import CheckboxOpcions from './components/CheckboxOpcions.vue';
+import FormularioRegistro from './components/FormularioRegistro.vue';
 
 export default {
   name: 'App',
   components: {
-    InputTexto,
-    BotonFormulario,
-    CajaSombreada,
-    CheckboxOpcions
+    FormularioRegistro,
   }
 }
 </script>
---
+++ frontend/src/components/FormularioRegistro.vue
@@ -0,0 +1,31 @@
+<template>
+    <CajaSombreada>
+        <h1>Formulario de registro</h1>
+
+        <InputTexto texto="Nombre de usuario" />
+        <InputTexto texto="Primer Apellido" />
+        <InputTexto texto="Segundo Apellido" />
+        <InputTexto texto="Fecha de nacimiento" />
+        <CheckboxOpcions texto="Sexo:" opcion1="M" opcion2="F" />
+        <InputTexto texto="Correo Electronico" />
+        <InputTexto texto="Contraseña" />
+        <InputTexto texto="Repetir contraseña" />
+        <BotonFormulario texto="Registrarse" />
+
+
+    </CajaSombreada>
+
+</template>
+<script>
+import CajaSombreada from './CajaSombreada.vue';
+import InputTexto from './InputTexto.vue';
+import CheckboxOpcions from './CheckboxOpcions.vue';
+import BotonFormulario from './BotonFormulario.vue';
+
+export default {
+    name: "FormularioRegistro",
+    props: {},
+    components: { CajaSombreada, CheckboxOpcions, InputTexto, BotonFormulario }
+}
+</script>
+<style></style>
